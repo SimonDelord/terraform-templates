c4674096e21331ab2e0d922726352c1e15b45f7a
SimonDelord <simon.delord@gmail.com>
2023-03-01 11:42:29 +1100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+
+variable "aws_zone" {
+  description = "AWS AZ"
+  type        = string
+  default     = "ap-southeast-1a"
+}
